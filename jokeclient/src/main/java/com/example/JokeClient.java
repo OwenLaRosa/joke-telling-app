b80f419f99d915df235e06971fc200448a1bb714
OwenLaRosa <owen.larosa@outlook.com>
2016-10-06 19:10:51 -0400

    feat: Add library for providing jokes

---
+++ jokeclient/src/main/java/com/example/JokeClient.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class JokeClient {
+
+    /**
+     * Get a joke
+     * Currently, the joke is always the same.
+     * @return A bad Halloween joke
+     */
+    public String getJoke() {
+        return "Why did the ghost go into the bar?\n\nFor the Boos!";
+    }
+
+}
