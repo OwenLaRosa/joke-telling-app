62427e7793ad76ee4b0940fb95a5d7a8127537b5
OwenLaRosa <owen.larosa@outlook.com>
2016-10-10 10:33:44 -0400

    feat: Fetch joke from server

--- jokeclient/src/main/java/com/example/JokeClient.java
+++ jokeclient/src/main/java/com/example/JokeClient.java
@@ -1,14 +1,40 @@
 package com.example;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class JokeClient {
+
+    private OkHttpClient mClient = new OkHttpClient();
+    private static final String BASE_URL = "http://localhost:8080/_ah/api/myApi/v1/";
+
+    // methods
+    private static final String JOKE = "mybean";
+
+    // json keys
+    private static final String JOKE_KEY = "data";
 
     /**
      * Get a joke
      * Currently, the joke is always the same.
      * @return A bad Halloween joke
      */
-    public String getJoke() {
-        return "Why did the ghost go into the bar?\n\nFor the Boos!";
+    public String getJoke() throws IOException, JSONException {
+        // fetch data from the server
+        String urlString = BASE_URL + JOKE;
+        Request request = new Request.Builder().url(urlString).build();
+        Response response = mClient.newCall(request).execute();
+        String result = response.body().string();
+
+        // extract the joke
+        JSONObject jsonObject = new JSONObject(result);
+        return jsonObject.getString(JOKE_KEY);
     }
 
 }
